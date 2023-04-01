1c74657b0d36a1052b16ce1c565f0b6259a5b4bb
Zheng, Lei <realthunder.dev@gmail.com>
2023-04-01 18:31:20 +0800

    Test conda cross compile

--- conda/01-soqt/build.sh
+++ conda/01-soqt/build.sh
@@ -1,12 +1,22 @@
 mkdir -p build
 cd build
+set -x
 
-cmake -G "Ninja" \
+declare -a CMAKE_PLATFORM_FLAGS
+
+if [[ ${HOST} =~ .*linux.* ]] ; then
+    if test ${CONDA_BUILD_CROSS_COMPILATION}; then
+        CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+    fi
+fi
+
+cmake --debug-find --debug-output -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D CMAKE_BUILD_TYPE="Release" \
       -D SOQT_USE_QT5=ON \
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO \
+      ${CMAKE_PLATFORM_FLAGS[@]} \
       ..
 
 ninja install
